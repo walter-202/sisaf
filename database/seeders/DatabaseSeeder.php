65a3c46832293cf1200c9d21132b855079d99946
walter-202 <wfernando.aguilarm@gmail.com>
2023-12-05 21:17:56 -0400

    Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,20 +31,5 @@ class DatabaseSeeder extends Seeder
             ServiciosTableSeeder::class,
             AuditsTableSeeder::class,
         ]);
-        $this->call(DataRowsTableSeeder::class);
-        $this->call(CitasTableSeeder::class);
-        $this->call(DataTypesTableSeeder::class);
-        $this->call(MenusTableSeeder::class);
-        $this->call(MenuItemsTableSeeder::class);
-        $this->call(SettingsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(UserRolesTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
-        $this->call(AuditsTableSeeder::class);
-        $this->call(MaterialesTableSeeder::class);
-        $this->call(MedicamentosTableSeeder::class);
-        $this->call(PacientesTableSeeder::class);
     }
 }
